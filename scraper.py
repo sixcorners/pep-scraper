f6944e07b325dc3e68b9ee12afed529eaa8bc021
sixcorners <sixcorners@gmail.com>
2017-05-06 18:06:29 -0500

    Add beginnings of scraper functionality.

--- scraper.py
+++ scraper.py
@@ -1,24 +1,21 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import scraperwiki
+import lxml.html
+import os
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+def parse_header(root):
+    data = {}
+    for row in root.cssselect(".header tr"):
+        td = row.cssselect("th,td")
+        data[td[0].text_content().strip(" \t\r\n:")] = td[1].text_content().strip()
+    return data
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+if __name__ == "__main__":
+    os.environ["SCRAPERWIKI_DATABASE_NAME"] = "sqlite:///data.sqlite"
+    tree = lxml.html.parse("https://www.python.org/dev/peps/")
+    scraperwiki.sqlite.save(unique_keys=['PEP'], data=parse_header(root))
+
+    for element, attribute, link, pos in tree.iterlinks():
+        if link.
+
+    scraperwiki.sql.execute("drop view if exists data")
+    scraperwiki.sql.execute("create view data as select * from swdata order by pep")
