1506f284039e76a2d8921a31715df7d9d8abaedb
sixcorners <sixcorners@gmail.com>
2017-05-07 01:21:08 -0500

    Small fixes.
    Filter out links with fragment parts.
    Fetch URLs in the order they are seen.

--- scraper.py
+++ scraper.py
@@ -4,30 +4,30 @@ import scraperwiki
 import lxml.html
 import re
 
-url_pattern = re.compile(".*/dev/peps/pep-.*")
+url_pattern = re.compile("/dev/peps/pep-\d*/?$")
 
 def parse_header(root):
     data = {}
-    for row in root.cssselect(".header tr, .field-list tr"):
+    for row in root.cssselect(".header, .field-list")[0].cssselect("tr"):
         td = row.cssselect("th, td")
         data[td[0].text_content().strip(" \t\r\n:")] = td[1].text_content().strip()
     print("Parsed", data)
     return data
 
 if __name__ == "__main__":
-    print("Fetching", "https://www.python.org/dev/peps/")
-    tree = lxml.html.fromstring(scraperwiki.scrape("https://www.python.org/dev/peps/"))
+    startingLink = "https://www.python.org/dev/peps/"
+    print("Fetching", startingLink)
+    tree = lxml.html.fromstring(scraperwiki.scrape(startingLink))
     scraperwiki.sqlite.save(unique_keys=['PEP'], data=parse_header(tree))
 
-    tree.make_links_absolute("https://www.python.org/dev/peps/")
+    tree.make_links_absolute(startingLink)
     links = set()
     for element, attribute, link, pos in tree.iterlinks():
-        if url_pattern.match(link):
+        if url_pattern.search(link) and link not in links:
             links.add(link)
-    for link in links:
-        print("Fetching", link)
-        tree = lxml.html.fromstring(scraperwiki.scrape(link))
-        scraperwiki.sqlite.save(unique_keys=['PEP'], data=parse_header(tree))
+            print("Fetching", link)
+            tree = lxml.html.fromstring(scraperwiki.scrape(link))
+            scraperwiki.sqlite.save(unique_keys=['PEP'], data=parse_header(tree))
 
     scraperwiki.sql.execute("drop view if exists data")
     scraperwiki.sql.execute("create view data as select * from swdata order by pep")
